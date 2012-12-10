a4f5e7fc329218c66797da7f996853fe4af77593
Roman Iakymchuk <roman.iakymchuk@telecom-sudparis.eu>
2012-12-10 10:16:20 +0100

    Created a separate executable evnt_read, which works similar to fxt_print; changed Makefile accordingly.

--- src/evnt_read.c
+++ src/evnt_read.c
@@ -173,3 +173,50 @@ void close_trace(evnt_trace_t* buffer) {
     __buffer_ptr = NULL;
     *buffer = NULL;
 }
+
+int main(int argc, const char **argv) {
+    evnt_size_t i;
+    const char* filename = "trace";
+    evnt_t* event;
+    evnt_trace_t buffer;
+
+    if ((argc == 3) && (strcmp(argv[1], "-f") == 0))
+        filename = argv[2];
+    else {
+        fprintf(stderr, "Specify the name of the trace file using '-f'\n");
+        exit(-1);
+    }
+
+    buffer = open_trace(filename);
+    printf("=============================================================\n");
+    printf("Printing events from the %s file\n\n", filename);
+    printf("Event Code \t Thread ID \t Time \t NB args \t Arguments (0-9)\n");
+
+    while (buffer != NULL ) {
+        event = read_event(&buffer);
+
+        if (event == NULL )
+            break;
+
+        if (get_bit(event->code) == 0) {
+            // regular event
+            printf("%lu \t %lu \t %lu \t %lu", event->code, event->tid, event->time, event->nb_params);
+
+            for (i = 0; i < event->nb_params; i++)
+                printf("\t %lu", event->param[i]);
+        } else {
+            // raw event
+            event->code = clear_bit(event->code);
+
+            printf("%lu \t %lu \t %lu \t %lu", event->code, event->tid, event->time, event->nb_params);
+            printf("\t %s", (evnt_data_t *) event->param);
+        }
+
+        printf("\n");
+    }
+
+    printf("=============================================================\n");
+    close_trace(&buffer);
+
+    return EXIT_SUCCESS;
+}
