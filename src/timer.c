e424e6f362c4c123d493db437dc66a4e63cfe552
Roman Iakymchuk <roman.iakymchuk@telecom-sudparis.eu>
2013-02-26 15:35:41 +0100

    - Removed dublication of evnt_time_t; - Rearanged code a bit.

--- src/timer.c
+++ src/timer.c
@@ -14,34 +14,55 @@
     abort();								\
   } while(0)
 
-
-/* choose the default timing method */
+// Choose the default timing method
 #ifdef CLOCK_GETTIME_AVAIL
 #ifdef CLOCK_MONOTONIC_RAW
-  #define TIMER_DEFAULT evnt_get_time_monotonic_raw
+#define TIMER_DEFAULT evnt_get_time_monotonic_raw
 #else
-  #define TIMER_DEFAULT evnt_get_time_monotonic
-#endif	/* CLOCK_MONOTONIC_RAW */
+#define TIMER_DEFAULT evnt_get_time_monotonic
+#endif	// CLOCK_MONOTONIC_RAW
+#else  // CLOCK_GETTIME_AVAIL
+#define TIMER_DEFAULT evnt_get_time_ticks
+#endif // CLOCK_GETTIME_AVAIL
 
-#else  /* CLOCK_GETTIME_AVAIL */
-  #define TIMER_DEFAULT evnt_get_time_ticks
-#endif /* CLOCK_GETTIME_AVAIL */
-
-
-/* Selected timimng method */
+/*
+ * Selected timing method
+ */
 evnt_timing_method_t evnt_get_time = TIMER_DEFAULT;
 
-
-/* return -1 if not available. return 0 otherwise */
-int evnt_set_timing_method(evnt_timing_method_t callback) {
-  if(!callback)
-    return -1;
-
-  evnt_get_time = callback;
-  return 0;
+/*
+ * This function initializes the timing mechanism
+ */
+void evnt_time_initialize() {
+    char* time_str = getenv("EVNT_TIMING_METHOD");
+    if (time_str) {
+        if (strcmp(time_str, "monotonic_raw") == 0)
+            evnt_set_timing_method(evnt_get_time_monotonic_raw);
+        else if (strcmp(time_str, "monotonic") == 0)
+            evnt_set_timing_method(evnt_get_time_monotonic);
+        else if (strcmp(time_str, "ticks") == 0)
+            evnt_set_timing_method(evnt_get_time_ticks);
+        else {
+            fprintf(stderr, "Unknown timining method: '%s'\n", time_str);
+            abort();
+        }
+    }
 }
 
+/*
+ * This function returns -1 if none of timings is available. Otherwise, it returns 0
+ */
+int evnt_set_timing_method(evnt_timing_method_t callback) {
+    if (!callback)
+        return -1;
 
+    evnt_get_time = callback;
+    return 0;
+}
+
+/*
+ * This function uses clock_gettime(CLOCK_MONOTONIC_RAW)
+ */
 evnt_time_t evnt_get_time_monotonic_raw() {
 #if(defined(CLOCK_GETTIME_AVAIL) && defined( CLOCK_MONOTONIC_RAW))
     evnt_time_t time;
@@ -55,7 +76,9 @@ evnt_time_t evnt_get_time_monotonic_raw() {
 #endif
 }
 
-
+/*
+ * This function uses clock_gettime(CLOCK_MONOTONIC)
+ */
 evnt_time_t evnt_get_time_monotonic() {
 #ifdef CLOCK_GETTIME_AVAIL
     evnt_time_t time;
@@ -69,12 +92,12 @@ evnt_time_t evnt_get_time_monotonic() {
 #endif
 }
 
+/*
+ * This function uses CPU specific register (for instance, rdtsc for X86* processors)
+ */
 evnt_time_t evnt_get_time_ticks() {
-
 #if HAVE_RDTSC
-  /*
-   * This is a copy of rdtscll function from asm/msr.h
-   */
+     // This is a copy of rdtscll function from asm/msr.h
 #define ticks(val) do {					\
     uint32_t __a,__d;						\
     asm volatile("rdtsc" : "=a" (__a), "=d" (__d));		\
@@ -86,38 +109,20 @@ evnt_time_t evnt_get_time_ticks() {
 #define ticks(val) (val) = -1
 #endif
 
+    static int ticks_initialized = 0;
+    static evnt_time_t __ticks_per_sec = -1;
+    if (!ticks_initialized) {
+        evnt_time_t init_start, init_end;
+        ticks(init_start);
+        usleep(1000000);
+        ticks(init_end);
 
-  static int ticks_initialized = 0;
-  static evnt_time_t __ticks_per_sec = -1;
-  if(!ticks_initialized) {
-    evnt_time_t init_start, init_end;
-    ticks(init_start);
-    usleep(1000000);
-    ticks(init_end);
-
-    __ticks_per_sec = init_end - init_start;
-    ticks_initialized = 1;
-  }
-
-  evnt_time_t time;
-  ticks(time);
-
-  return time*1e9/__ticks_per_sec;
-}
-
-
-void evnt_time_initialize() {
-  char* time_str = getenv("EVNT_TIMING_METHOD");
-  if(time_str) {
-    if(strcmp(time_str, "monotonic_raw") == 0)
-      evnt_set_timing_method(evnt_get_time_monotonic_raw);
-    else if(strcmp(time_str, "monotonic") == 0)
-      evnt_set_timing_method(evnt_get_time_monotonic);
-    else if(strcmp(time_str, "ticks") == 0 )
-      evnt_set_timing_method(evnt_get_time_ticks);
-    else {
-      fprintf(stderr, "Unknown timining method: '%s'\n", time_str);
-      abort();
+        __ticks_per_sec = init_end - init_start;
+        ticks_initialized = 1;
     }
-  }
+
+    evnt_time_t time;
+    ticks(time);
+
+    return time * 1e9 / __ticks_per_sec;
 }
--- src/timer.h
+++ src/timer.h
@@ -9,41 +9,44 @@
 
 #include "evnt_types.h"
 
-/* time in nanosecond */
-typedef uint64_t evnt_time_t;
-
-
-/* Callback function that return the current time in ns.
+/*
+ * Callback function that return the current time in ns.
  * It can be either a pointer to one of the timing functions
  * provided by libevnt, or a user-defined function.
  */
-typedef evnt_time_t (*evnt_timing_method_t)() ;
+typedef evnt_time_t (*evnt_timing_method_t)();
 
-
-/* Calls the selected timing method and get the current time in ns */
+/*
+ * This function calls the selected timing method and get the current time in ns
+ */
 extern evnt_timing_method_t evnt_get_time;
 
-
-/* Function to initialize the timing mechanism */
+/*
+ * This function initializes the timing mechanism
+ */
 void evnt_time_initialize();
 
-/* Select the timing function to use.
+/*
+ * Select the timing function to use.
  * Return -1 if an error occurs
  */
 int evnt_set_timing_method(evnt_timing_method_t callback);
 
+// pre-defined timing methods:
 
-
-
-/*** pre-defined timing methods: ***/
-
-/* uses clock_gettime(CLOCK_MONOTONIC_RAW) */
+/*
+ * This function uses clock_gettime(CLOCK_MONOTONIC_RAW)
+ */
 evnt_time_t evnt_get_time_monotonic_raw();
 
-/* uses clock_gettime(CLOCK_MONOTONIC) */
+/*
+ * This function uses clock_gettime(CLOCK_MONOTONIC)
+ */
 evnt_time_t evnt_get_time_monotonic();
 
-/* uses CPU specific register (for instance, rdtsc for X86* processors) */
+/*
+ * This function uses CPU specific register (for instance, rdtsc for X86* processors)
+ */
 evnt_time_t evnt_get_time_ticks();
 
 #endif /* TIMER_H_ */
