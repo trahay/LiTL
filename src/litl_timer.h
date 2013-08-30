d18cdee504f99121c2d6d178d437ed22620e7569
roman <roman.iakymchuk@telecom-sudparis.eu>
2013-08-30 22:39:29 +0200

    Added doxygen comments for litl_timer.h

--- src/litl_timer.h
+++ src/litl_timer.h
@@ -7,60 +7,99 @@
 #ifndef LITL_TIMER_H_
 #define LITL_TIMER_H_
 
+/**
+ *  \file litl_timer.h
+ *  \brief litl_timer Provides a set of functions for measuring time
+ *
+ *  \authors
+ *    Developers are : \n
+ *        Roman Iakymchuk   -- roman.iakymchuk@telecom-sudparis.eu \n
+ *        Francois Trahay   -- francois.trahay@telecom-sudparis.eu \n
+ */
+
 #include "litl_types.h"
 
-/*
- * A callback function that returns the current time in ns.
- * It can be either a pointer to one of the timing functions
- * provided by LiTL or a user-defined function
+/**
+ * \defgroup litl_timer LiTL Timing Methods
+ */
+
+/**
+ * \defgroup litl_timer_init Initialization Functions
+ * \ingroup litl_timer
+ */
+
+/**
+ * \defgroup litl_timer_measure Pre-Defined Timing Methods
+ * \ingroup litl_timer
+ */
+
+/**
+ * \ingroup litl_timer_init
+ * \brief A callback function that returns the current time in ns. It can be either a pointer to one of the timing functions provided by LiTL or a user-defined function
  */
 typedef litl_time_t (*litl_timing_method_t)();
 
-/*
- * Calls the selected timing method and get the current time in ns
+/**
+ * \ingroup litl_timer_measure
+ * \brief Calls the selected timing method and get the current time in ns
  */
 extern litl_timing_method_t litl_get_time;
 
-/*
- * Initializes the timing mechanism
+/**
+ * \ingroup litl_timer_init
+ * \brief Initializes the timing mechanism
  */
 void litl_time_initialize();
 
-/*
- * Selects the timing function to use.
- * Return -1 if an error occurs
+/**
+ * \ingroup litl_timer_init
+ * \brief Selects the timing function to use
+ * \param callback A name of timing function
+ * \return Returns -1 if an error occurs. Otherwise, returns 0
  */
 int litl_set_timing_method(litl_timing_method_t callback);
 
 // Pre-defined timing methods:
 
-/*
- * Uses clock_gettime(CLOCK_MONOTONIC_RAW)
+/**
+ * \ingroup litl_timer_measure
+ * \brief Uses clock_gettime(CLOCK_MONOTONIC_RAW)
+ * \return Returns time that is similar to CLOCK_MONOTONIC, but provides access to a raw hardware-based time
  */
 litl_time_t litl_get_time_monotonic_raw();
 
-/*
- * Uses clock_gettime(CLOCK_MONOTONIC)
+/**
+ * \ingroup litl_timer_measure
+ * \brief Uses clock_gettime(CLOCK_MONOTONIC)
+ * \return Returns the monotonic time since some unspecified starting point
  */
 litl_time_t litl_get_time_monotonic();
 
-/*
- * Uses clock_gettime(CLOCK_REALTIME)
+/**
+ * \ingroup litl_timer_measure
+ * \brief Uses clock_gettime(CLOCK_REALTIME)
+ * \return Returns the real (wall-clock) time
  */
 litl_time_t litl_get_time_realtime();
 
-/*
- * Uses clock_gettime(CLOCK_PROCESS_CPUTIME)
+/**
+ * \ingroup litl_timer_measure
+ * \brief Uses clock_gettime(CLOCK_PROCESS_CPUTIME)
+ * \return Returns the high-resolution per-process time from the CPU
  */
 litl_time_t litl_get_time_process_cputime();
 
-/*
- * Uses clock_gettime(CLOCK_THREAD_CPUTIME)
+/**
+ * \ingroup litl_timer_measure
+ * \brief Uses clock_gettime(CLOCK_THREAD_CPUTIME)
+ * \return Returns the thread-specific CPU-time
  */
 litl_time_t litl_get_time_thread_cputime();
 
-/*
- * Uses CPU specific register (for instance, rdtsc for X86* processors)
+/**
+ * \ingroup litl_timer_measure
+ * \brief Uses CPU-specific register (for instance, rdtsc for X86* processors)
+ * \return Returns the measured time in clock cycles
  */
 litl_time_t litl_get_time_ticks();
 
