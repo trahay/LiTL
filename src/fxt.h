618464516daada2147a1f1eece55b76197d5b23d
roman <roman.iakymchuk@telecom-sudparis.eu>
2013-09-03 11:15:45 +0200

    Finished implementing mapping between LiTL and FxT APIs

--- src/fxt.h
+++ src/fxt.h
@@ -24,53 +24,55 @@
 #include "litl_write.h"
 #include "litl_read.h"
 
-typedef struct litl_trace_write_t *fxt_t;
+typedef litl_trace_write_t* fxt_t;
 
-fxt_t litl_trace;
+static fxt_t __trace;
 
-/* BEGIN -- Recording events*/
-// here is the main difference, see eztrace_core.c line:255
-int fut_setup(unsigned int nints, unsigned int keymask, unsigned int threadid);
+/* BEGIN -- Recording functions */
+#define fut_setup(buffer_size, keymask, threadid) do {          \
+      __trace = litl_write_init_trace(buffer_size);             \
+    }while(0)
 
 // finalizing traces
-#define fut_endup(filename) litl_fin_trace(litl_trace);
-#define fut_done(void) do {                            \
+#define fut_endup(filename) do {                                \
+      litl_write_finalize_trace(__trace);                       \
+    }while(0)
+#define fut_done(void) do {                                     \
     } while(0)
 
-#define fut_set_filename(filename) do {                \
-      litl_write_set_filename(trace, filename);        \
+#define fut_set_filename(filename) do {                         \
+      litl_write_set_filename(__trace, filename);               \
     } while(0)
 
-#define enable_fut_flush() do {                        \
-      litl_write_buffer_flush_on(trace);               \
+#define enable_fut_flush() do {                                 \
+      litl_write_buffer_flush_on(__trace);                      \
     } while(0)
 
-#define disable_fut_flush() do {                       \
-      litl_write_buffer_flush_off(trace);              \
+#define disable_fut_flush() do {                                \
+      litl_write_buffer_flush_off(__trace);                     \
     } while(0)
 
-#define fut_enable_tid_logging() do {                  \
-      litl_write_tid_recording_on(trace);              \
+#define fut_enable_tid_logging() do {                           \
+      litl_write_tid_recording_on(__trace);                     \
     } while(0)
 
-#define fut_disable_tid_logging() do {                 \
-      litl_write_tid_recording_off(trace);             \
+#define fut_disable_tid_logging() do {                          \
+      litl_write_tid_recording_off(__trace);                    \
     } while(0)
+
+/* END -- Recording functions */
 
 /* BEGIN -- Events */
-
-#define FUT_DO_PROBE0(code) litl_probe_pack_0(litl_trace, code)
-#define FUT_DO_PROBE1(code, arg1) litl_probe_pack_1(litl_trace, code, arg1)
-#define FUT_DO_PROBE2(code, arg1, arg2) litl_probe_pack_2(litl_trace, code, arg1, arg2)
-#define FUT_DO_PROBE3(code, arg1, arg2, arg3) litl_probe_pack_3(litl_trace, code, arg1, arg2, arg3)
-#define FUT_DO_PROBE4(code, arg1, arg2, arg3, arg4) litl_probe_pack_4(litl_trace, code, arg1, arg2, arg3, arg4)
-#define FUT_DO_PROBE5(code, arg1, arg2, arg3, arg4, arg5) litl_probe_pack_5(litl_trace, code, arg1, arg2, arg3, arg4, arg5)
-#define FUT_DO_PROBE6(code, arg1, arg2, arg3, arg4, arg5, arg6) litl_probe_pack_6(litl_trace, code, arg1, arg2, arg3, arg4, arg5, arg6)
-#define FUT_DO_PROBE(code, ...) litl_probe_pack_0(litl_trace, code)
-#define FUT_DO_PROBESTR(code, str) litl_probe_raw(litl_trace, code, strlen(str), str)
+#define FUT_DO_PROBE0(code) litl_write_probe_pack_0(__trace, code)
+#define FUT_DO_PROBE1(code, arg1) litl_write_probe_pack_1(__trace, code, arg1)
+#define FUT_DO_PROBE2(code, arg1, arg2) litl_write_probe_pack_2(__trace, code, arg1, arg2)
+#define FUT_DO_PROBE3(code, arg1, arg2, arg3) litl_write_probe_pack_3(__trace, code, arg1, arg2, arg3)
+#define FUT_DO_PROBE4(code, arg1, arg2, arg3, arg4) litl_write_probe_pack_4(__trace, code, arg1, arg2, arg3, arg4)
+#define FUT_DO_PROBE5(code, arg1, arg2, arg3, arg4, arg5) litl_write_probe_pack_5(__trace, code, arg1, arg2, arg3, arg4, arg5)
+#define FUT_DO_PROBE6(code, arg1, arg2, arg3, arg4, arg5, arg6) litl_write_probe_pack_6(__trace, code, arg1, arg2, arg3, arg4, arg5, arg6)
+#define FUT_DO_PROBE(code, ...) litl_write_probe_pack_0(__trace, code)
+#define FUT_DO_PROBESTR(code, str) litl_write_probe_raw(__trace, code, strlen(str), str)
 
 /* END -- Events */
-
-/* END -- Recording events */
 
 #endif /* FXT_H_ */
